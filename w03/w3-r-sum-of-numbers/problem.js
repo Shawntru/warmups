22abfc85decaf3fe2653e4578b8621687bbd9c4c
Shawn Truesdale <shawnctruesdale@gmail.com>
2021-04-02 10:52:50 -0600

    Add solution for sum of nums problem

--- w03/w3-r-sum-of-numbers/problem.js
+++ w03/w3-r-sum-of-numbers/problem.js
@@ -17,7 +17,27 @@ Psuedo:
 Have: 2 arguments, integers
 Want: Sum of all integers between them
 
-
+Declare individual values by creating input array with both arg input, and sort array
+Declare sum var as 0
+Instantiate while loop
+  condition 1st arg is < 2nd arg
+  sum = 1st arg + 1st arg ++
+Return sum
 */
 
-function getSum(a, b) {}
+function getSum(a, b) {
+  if (a === b) return a;
+  let sortedInputs = [a, b].sort((a, b) => (a > b ? a : b));
+  let lowNum = sortedInputs[0];
+  let highNum = sortedInputs[1];
+  let sum = 0;
+  while (lowNum <= highNum) {
+    sum += lowNum;
+    lowNum++;
+  }
+  return sum;
+}
+
+module.exports = {
+  getSum: getSum,
+};
